ecb3e40640ff80606c88c2d6241ac9fd20a0223d
Nilton Jr <niltonfjnr@hotmail.com>
2023-02-23 23:18:09 -0300

    refactor: cleaning unused code

--- src/main/index.js
+++ src/main/index.js
@@ -1,8 +1,5 @@
 import { LoadPipeline } from './load';
 
-// eslint-disable-next-line no-undef
-global.process.env.NODE_ENV = 'production';
-
 // EXTRACT/TRANSFORM/LOAD
 // FULL PIPELINE
 const main = () => {
